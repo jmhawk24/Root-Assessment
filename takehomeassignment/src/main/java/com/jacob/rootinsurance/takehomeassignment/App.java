51521fb3a16db4c8393023334e699f676b8cf687
Jacob Hawk <hawk.music24@gmail.com>
2019-12-05 16:53:37 -0500

    starting root insurance assignment and init workspace

---
+++ takehomeassignment/src/main/java/com/jacob/rootinsurance/takehomeassignment/App.java
@@ -0,0 +1,19 @@
+package com.jacob.rootinsurance.takehomeassignment;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	
+        
+    }
+}
+
+
+class Driver {
+	public String name;
+	public double distance;
+	public double speed;
+}
